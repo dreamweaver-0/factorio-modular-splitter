e0ddc303a8294093e31d8df8d47a035fecf2c4d8
Michael Christensen <michjchrist@gmail.com>
2026-09-10 14:28:01 -0400

    initial setup

---
+++ settings.lua
@@ -0,0 +1,34 @@
+data:extend{
+    {
+        name = "manifold-splitter-mode"
+        type = "string-setting",
+        setting_type = "startup",
+        order = "a",
+        default_value = "normal",
+        allowed_values = {"normal", "lubricated"},
+    },
+    {
+        name = "manifold-splitter-cost-multiplier",
+        type = "double-setting",
+        setting_type = "startup",
+        order = "b",
+        default_value = 1.0,
+        allowed_values = {0.25, 0.5, 1.0, 2.0, 4.0}
+    },
+    {
+        name = "manifold-splitter-extension-modes",
+        type = "string-setting",
+        setting_type = "startup",
+        order = "c",
+        default_value = "scaling",
+        allowed_values = {"scaling", "fixed", "infinite"}
+    },
+    {
+        name = "manifold-splitter-extension-limit-multiplier",
+        type = "double-setting",
+        setting_type = "startup",
+        order = "d",
+        default_value = 1.0,
+        allowed_values = {0.5, 1.0, 2.0, 4.0}
+    }
+}
